b1e77a67faf2f9ffa3d903a5c33551d7c7ea4ea5
Cayden Wagner <cayden.wagner@gmail.com>
2021-02-05 13:30:01 -0800

    Test

--- 01_Cryptography/main.cpp
+++ 01_Cryptography/main.cpp
@@ -1,1 +1,18 @@
-// Add your code here
+#include <iostream>
+
+using namespace std;
+
+void printChars(char character, int numChars);
+
+int main() {
+	printChars(*, 34);
+
+	return EXIT_SUCCESS;
+}
+
+void printChars(char character, int numChars) {
+	for (int i = 0; i < numChars; i++) {
+		cout << character;
+	}
+	cout << endl;
+}
